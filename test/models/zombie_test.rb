5178c345251074b470bd75fb98223eec540d400a
Kshitij Limaye <klimaye@gmail.com>
2014-10-20 22:20:15 -0500

    added test for the zombie model

--- test/models/zombie_test.rb
+++ test/models/zombie_test.rb
@@ -4,4 +4,16 @@ class ZombieTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  def test_invalid_without_a_name
+    z = Zombie.new
+    assert !z.valid?, "Name is not being validated"
+  end
+
+  test "valid with all attributes" do
+    z = Zombie.new
+    z.name = "Prajya"
+    z.age = 32
+    assert z.valid?, "Zombie not valid"
+  end
 end
